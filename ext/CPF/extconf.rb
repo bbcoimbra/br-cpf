fc8717dbd5de0a2d226a9de92297361665386c13
Bruno Coimbra <bbcoimbra@gmail.com>
2010-04-16 18:36:00 -0300

    fix -D_FILE_OFFSET_BITS in i[3456]86 architetures

--- ext/CPF/extconf.rb
+++ ext/CPF/extconf.rb
@@ -6,6 +6,7 @@ have_header("stdlib.h")
 have_func('lldiv')
 have_type('lldiv_t','stdlib.h')
 
-with_cflags('-std=c99') do
+cflags = '-std=c99 -U_FILE_OFFSET_BITS'
+with_cflags(cflags) do
   create_makefile("CPF")
 end
