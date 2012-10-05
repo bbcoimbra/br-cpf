dd043436b815e140f67b35f39a297b15a3d58783
Nelson Haraguchi <nelsonmhjr@gmail.com>
2012-10-05 16:09:15 -0300

    Fix load of .bundle on Mac OSX. fix #7

--- lib/br/cpf/cpf_wrapper.rb
+++ lib/br/cpf/cpf_wrapper.rb
@@ -2,7 +2,10 @@ require 'ffi'
 module BR
   module CPFWrapper
     extend FFI::Library
-    ffi_lib File.join(File.dirname(__FILE__), "..", "..", "CPF.so")
+    path = File.join(File.dirname(__FILE__), "..", "..")
+    lib = File.join(path, 'CPF.so')
+    lib = File.join(path, 'CPF.bundle') unless File.exists?(lib)
+    ffi_lib lib
     attach_function :calc_check_digit, [:long_long], :long_long
     attach_function :is_valid, [:long_long], :bool
   end
